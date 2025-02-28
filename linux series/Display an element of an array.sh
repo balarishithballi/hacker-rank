5b9e30843e4b23a65d09020488305a0dd10f9a47
Balarishith balli <balarishith.balli.cys@gmail.com>
2025-02-28 21:30:16 +0530

    Delete linux series/Display an element of an array.sh

--- linux series/Display an element of an array.sh
+++
@@ -1,3 +0,0 @@
-# Given a list of countries, each on a new line, your task is to read them into an array and then display the element indexed at . Note that indexing starts from 0.
-arr=($(cat))
-echo ${arr[3]}
